eb883185ce6d6dc006e658e0328cc6b709f53b2e
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-06-15 09:39:07 +0300

    Firebase
    ödeme bilgilerinin veri tabanına aktarılması hususunda yapılan işlemlerin bulunduğu servis.

---
+++ DBFirebase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using eShopOnContainers.Core.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+using MvvmHelpers;
+using eShopOnContainers.Core.Services.Odeme;
+
+using Xamarin.Forms;
+
+
+namespace eShopOnContainers.Core.Services.Odeme
+{
+	public class DBFirebase
+	{
+		FireBaseClient client;
+
+		public DBFirebase()
+        {
+			client = new FireBaseClient("https://odemebilgi-f6799-default-rtdb.firebaseio.com/");
+        }
+
+		public ObservableCollection<Employee> getEmployee()
+        {
+
+			var odemeveri = client
+				.Child("Employees")
+				.AsObservable<Employee>()
+				.AsObservableCollection();
+
+			return odemeveri;
+        }
+
+		public async Task AddEmployee(string ad,string soyad,string kartNo ,int cVC)
+        {
+			Employee e = new Employee() { Ad = ad, Soyad = soyad, KartNo = kartNo, CVC = cVC };
+			await client
+				.Child("Employees")
+				.PostAsync(e);
+
+
+        }
+
+	}
+}
+
