e49557dba2256af09c2d1cebff45cad5d4630207
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-06-15 09:31:31 +0300

    Ödemeİnfo
    bu kısımda ödeme bilgileini karşı taraftan alıp kaydediyorum.

---
+++ Employee/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace eShopOnContainers.Core.Models
+{
+	public class Employee
+	{
+		
+	    public string Ad { get; set; }
+		public string Soyad { get; set; }
+		public string KartNo { get; set; }
+		public int CVC { get; set; }
+	
+	}
+}
+
