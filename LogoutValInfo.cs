6943ead53298f3ddce6547d2496db327ea88e581
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-06-23 23:10:46 +0300

    Çıkış bilgileri tutma
    profil kısmından çıkış yaparken giriş bilgilerini tutuyor

---
+++ LogoutValInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopOnContainers.Core.Models.Logouts
+{
+    public class LogoutValInfo
+    {
+        public int userID { get; set; }
+        public string userPASS { get; set; }
+        public string userNAME { get; set; }
+
+        public int logoutNumber { get; set; }
+
+
+    }
+}
