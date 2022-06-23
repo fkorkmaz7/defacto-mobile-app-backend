a757e8d39161d18674e555cfe39f94ba88e1033e
fkorkmaz7 <104424302+fkorkmaz7@users.noreply.github.com>
2022-06-23 23:12:24 +0300

    çıkış bildirim mesajı infosu
    bildirim mesajında yer alacak bilgileri çektiğimiz sınıf

---
+++ LogoutsCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopOnContainers.Core.Models.Logouts
+{
+    public class LogoutsCommand
+    {
+
+        public int LogoutNumber { get; }
+
+        public LogoutsCommand(int logoutNumber)
+        {
+            LogoutNumber = logoutNumber;
+        }
+    }
+}
